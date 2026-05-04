66e4ffa1025f4ee539f25195fc9c4e0c68d11451
Djozman <121141050+Djozman@users.noreply.github.com>
2026-05-04 14:38:45 -0400

    Initial Commit

---
+++ Canopy/CanopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanopyApp.swift
+//  Canopy
+//
+//  Created by Amm on 2026-05-04.
+//
+
+import SwiftUI
+
+@main
+struct CanopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
