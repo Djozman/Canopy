75fbe8a311c845fa7d8eed72675e0645ac009f5a
Djozman <121141050+Djozman@users.noreply.github.com>
2026-05-05 22:48:30 -0400

    Phase 0 complete: ground truth info hash tests, remove copyrighted torrents

--- Tests/CanopyEngine/RealTorrentTests.swift
+++ Tests/CanopyEngine/RealTorrentTests.swift
@@ -3,6 +3,12 @@ import XCTest
 
 final class RealTorrentTests: XCTestCase {
 
+    private func torrentData(named filename: String) throws -> Data {
+        let dir = try XCTUnwrap(Bundle.module.resourceURL?.appendingPathComponent("TestTorrents"))
+        let url = dir.appendingPathComponent(filename)
+        return try Data(contentsOf: url)
+    }
+
     func testAllTestTorrentsParse() throws {
         let dir = try XCTUnwrap(Bundle.module.resourceURL?.appendingPathComponent("TestTorrents"))
         let files = try FileManager.default.contentsOfDirectory(at: dir, includingPropertiesForKeys: nil)
@@ -31,5 +37,20 @@ final class RealTorrentTests: XCTestCase {
             let b = try TorrentParser.parse(data: Data(contentsOf: url))
             XCTAssertEqual(a.infoHash, b.infoHash, "infoHash not stable for \(url.lastPathComponent)")
         }
+    }
+
+    func testInfoHashMatchesKnownValue_archlinux() throws {
+        let torrent = try TorrentParser.parse(data: torrentData(named: "archlinux-2026.05.01-x86_64.iso.torrent"))
+        XCTAssertEqual(torrent.infoHash.hexString, "e337a880c4d0f552bab5b437fe1208d26130ccc5")
+    }
+
+    func testInfoHashMatchesKnownValue_ubuntu() throws {
+        let torrent = try TorrentParser.parse(data: torrentData(named: "ubuntu-24.04.4-desktop-amd64.iso.torrent"))
+        XCTAssertEqual(torrent.infoHash.hexString, "01c137287d6f0ed05a56742dae794f632c79ff3d")
+    }
+
+    func testInfoHashMatchesKnownValue_debian() throws {
+        let torrent = try TorrentParser.parse(data: torrentData(named: "debian-13.4.0-amd64-netinst.iso.torrent"))
+        XCTAssertEqual(torrent.infoHash.hexString, "3b1de9cb7011350fa152ec47419620aa153e19e7")
     }
 }
